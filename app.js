d8f1e2f3a26d62df8ecbf665ed26f1e9781a4baa
Rafael <therafaeljr03@gmail.com>
2024-06-08 11:33:07 +0200

    fikk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const stripe = require("stripe")('sk_test_51PJ2BDP1QytsEo5aGArjBS7PoZgiafH5ZhN70Ycqznirp19y4wbbPKsE290P1IfOui7VmVIXgg3RnKwqSI7vqhGR00K9awoqQf');
+const stripe = require("stripe")('sk_test_51PJ2HrKTDh0OrkSjVzQE24cX5yGC6tg9FupdX8skNYu2YA8RNDUP6vxbHjNHCBLRsLG0gpROSM1EzNthUbkeqYuN004TckxaZm');
 const PORT = 3030;
 
 app.use(express.json());
--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")("sk_test_51PJ2BDP1QytsEo5aGArjBS7PoZgiafH5ZhN70Ycqznirp19y4wbbPKsE290P1IfOui7VmVIXgg3RnKwqSI7vqhGR00K9awoqQf");
+const stripe = require("stripe")("sk_test_51PJ2HrKTDh0OrkSjVzQE24cX5yGC6tg9FupdX8skNYu2YA8RNDUP6vxbHjNHCBLRsLG0gpROSM1EzNthUbkeqYuN004TckxaZm");
 
 exports.createPaymentSession = async (req, res) => {
   const { success_url, priceID, quantity } = req.body;
